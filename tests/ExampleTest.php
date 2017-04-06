a666ffb2ee763694450185926c7717fe0087b8aa
mihajah <xanaviarta@mail.com>
2017-04-06 18:35:52 +0200

    tests/ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -17,8 +17,13 @@ class ExampleTest extends TestCase {
 	public function testOneCollection()
 	{
 		$response = $this->call('GET', 'collections/1');
-		print_r($response->getContent());
+		print_r($response->getContent()); 
 		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	private static function watcher()
+	{
+		//
 	}
 
 }
