6d9b6e1756d69b04d08ae928c0a4109b91ae3af0
mihajah <xanaviarta@mail.com>
2017-07-06 13:25:15 +0200

    update DB cfg 2

--- database/database.php
+++ database/database.php
@@ -58,7 +58,7 @@ return [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', '127.0.0.1'),
 			'database'  => env('DB_DATABASE', 'touchiz'),
-			'username'  => env('DB_USERNAME', 'mihaja'),
+			'username'  => env('DB_USERNAME', 'root'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
